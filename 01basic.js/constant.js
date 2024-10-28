c085f24ed275cfe6a7e308ef5eebf8303aabd2f7
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-10-29 01:03:39 +0530

    add files

---
+++ 01basic.js/constant.js
@@ -0,0 +1,14 @@
+const accountId =145236
+let accountEmail ="priyanshukashyap@gmail,com"
+var accountPassword ="874466256"
+accountcity ="chapra bihar"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountcity]);
+
+ accountEmail ="kashyap@gmail,com"
+ accountPassword ="987512"
+accountcity ="DHANTEWARA"
+
+
+console.table([accountEmail,accountId,accountPassword,accountcity,accountState])
