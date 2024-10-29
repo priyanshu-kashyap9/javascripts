e08625653aedcd400bb4bf4922bcbdd34c83ba56
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-10-30 02:45:59 +0530

    add some code

---
+++ 01basic.js/conversion.js
@@ -0,0 +1,19 @@
+let age = "35abc"
+console.log(typeof age);
+console.log(typeof (age));
+
+let score =Number(age)
+console.log(typeof(score));
+console.log(score);
+
+
+
+let value ="78"
+let bollenvalue =Boolean(value)
+console.log(bollenvalue);
+
+let values =7854
+let stringnvalues =String(values)
+console.log(stringnvalues);
+console.log(typeof stringnvalues)
+
