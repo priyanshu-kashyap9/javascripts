728d21786d0f551d7b0aa16a1f86469e77570f15
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-10-31 02:03:55 +0530

    study expand

---
+++ 01basic.js/operation.js
@@ -0,0 +1,35 @@
+// this code is exuteing/usingfor changing the value in number given like negative to positive and viseversa
+let value =98
+let Negvalue = -value
+console.log(Negvalue);
+
+// some other operation in this code we are adding the two string 
+let str1 ="Hello"
+let str2 = " visiter"
+let str3 =str1+str2
+console.log(str3);
+
+// some other operation
+console.log(true);
+console.log(+true);
+console.log(+false);
+
+// other operation 
+console.log("8" + 2 +5);
+console.log(8 + 2 +"5");
+
+// last example of operation
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+// same exple per minor change 
+
+/* let gamecounter = 100
+ ++gamecounter;
+ console.log(gamecounter); */
+
+// in up code is explain it is error process in js its wrong .
+
+
+
