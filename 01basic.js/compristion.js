292843cc04de855cefec3060ddfa6f762fa340a5
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-10-31 13:23:52 +0530

    compristion

---
+++ 01basic.js/compristion.js
@@ -0,0 +1,28 @@
+// in this code with disscuse about compristion in data type 
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+console.log(2<1);
+
+// anothe type converrstion in js
+
+console.log("2"> 1);
+console.log(1 > "2");
+
+// up code is not allow  to use diffrent data types 
+
+// converstion in null and zero in down code 
+console.log(null > 0);
+console.log(null >=  0);
+console.log(null ==  0);
+
+// converstion in undfine and zero in down code 
+console.log(undefined > 0);
+console.log(undefined >=  0);
+console.log(undefined ==  0);
+
+// in the last "stikle check" the the convestion in check like string and all possibale confirm
+
+console.log("2" === 2);
