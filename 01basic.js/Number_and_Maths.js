081d5aacc3cd17b28c184d249ff4e90020911dec
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-11-03 02:48:00 +0530

    number and maths

---
+++ 01basic.js/Number_and_Maths.js
@@ -0,0 +1,43 @@
+// in the leacture we disscuss the number using in javascript 
+
+const score  = 400
+const balance = new Number(100)
+console.log(score);
+console.log(balance);
+
+// so we discuss the another function in nuber using according your need in life/code 
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(balance.toPrecision());
+
+const hundereds = 100000
+console.log(hundereds.toLocaleString());
+console.log(hundereds.toLocaleString('en-In'));
+
+
+// so another topic is maths in using the javasript 
+
+// MATHS :- MATHS IS THE LIBRARY OF JAVASCPIT AND MANY MORE LANGUGE IN CODEING 
+
+console.log(Math.abs(-4));           /* the abs function is use for change the value negative to positive */
+console.log(Math.round(-41.3));           /* the round  function is use for not get point value */
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,5,9,7,2,3,8));
+console.log(Math.max(8,4,9,0,1,3,5));
+console.log(Math.random());
+
+
+// give the max and min some example 
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min +1)) + min ) ;
+console.log((Math.random() * (max-min +1)));
+
+
+
+
+
+
