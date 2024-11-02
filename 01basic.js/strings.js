d8427901727f3e59e96603a55bc487890c834470
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-11-02 18:12:59 +0530

    add string types

---
+++ 01basic.js/strings.js
@@ -0,0 +1,20 @@
+// in the leacture we read string in the js
+const name = " priyanshu "
+const repo =  5
+console.log(" hello " + name + repo );
+
+// we use new method to use the same work but it understandebale in code 
+
+console.log(`${"hello"} i am ${name} kashyap .In github reposetry is ${repo}`);
+
+// some other example of the string in the use in js 
+ 
+const gameName = new String('hellom u');
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf("l"));
+
+
+
