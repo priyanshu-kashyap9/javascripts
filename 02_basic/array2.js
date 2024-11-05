bb8ce3619d8ed98d4af2f10fe42181708d901f37
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-11-05 23:47:17 +0530

    second part array

---
+++ 02_basic/array2.js
@@ -0,0 +1,7 @@
+// the some part of the array in second leacture in the array 
+
+
+const marvel_hero = ["thoe","ironman","spiderman"]
+const Dc_hero = ["superman","flash","batman"]
+marvel_hero.push(Dc_hero)
+console.log(marvel_hero);
