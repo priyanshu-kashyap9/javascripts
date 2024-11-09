2fde8cffc66d1adb19071fac19c7da49177cf3d7
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-11-09 23:01:16 +0530

    destructure

---
+++ 02_basic/de_structure.js
@@ -0,0 +1,23 @@
+// In the we study in the de_structure in object 
+
+const course ={
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "priyanshu kashyap"
+}
+const {courseInstructor : instrctuer } = course 
+console.log(instrctuer);
+
+
+
+// the api work like this way to thr achibve the data 
+/*
+{
+    "name" : "priyanshu",
+    "coursename" : "js in hindi",
+    "prize" : "free"
+}
+    */
+
+
+//    so the this part is over lets talk to new the content 
