8794271f14c0a6055c2da0ddf75cf13350708b3d
Priyanshu Kashyap <priyanshu24k@gmail.com>
2024-11-10 22:15:43 +0530

    function

---
+++ 03_basic/function.js
@@ -0,0 +1,28 @@
+function saymyname(){
+    console.log("P");
+    console.log("R");
+    console.log("I");
+    console.log("Y");
+    console.log("A");
+    console.log("N");
+    console.log("S");
+    console.log("H");
+    console.log("U");   
+}
+saymyname()
+
+function addtwo(number1,number2){
+    console.log(number1 + number2);
+}
+addtwo(3,5)
+
+function loginuser(username){
+    if(username===undefined){
+        console.log("please provide username");        
+    }
+    return`${username} jsut log in the site  `
+}
+
+console.log(loginuser("priyanshu"));
+
+// the first part is complete function in the js 
